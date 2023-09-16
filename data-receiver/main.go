0bcbc5eb4fe2ec0c1641ff9254c14d725d305279
othmanDev12 <omoudden545@gmail.com>
2023-09-16 20:50:10 +0100

    feat(kafka producer) :sparkles: put data from obu into kafka topics

--- data-receiver/main.go
+++ data-receiver/main.go
@@ -1,31 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/websocket"
-	"github.com/track-tracking/types"
 	"log"
 	"net/http"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/gorilla/websocket"
+	"github.com/track-tracking/types"
 )
 
+const KafkaTopic = "obudata"
+
 func main() {
-	recv := NewDataReceiver()
+	recv, err := NewDataReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/ws", recv.wsHandler)
-	err := http.ListenAndServe(":30000", nil)
+	err = http.ListenAndServe(":30000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func NewDataReceiver() *DataReceiver {
-	return &DataReceiver{
-		data: make(chan types.OBUData, 128),
+func NewDataReceiver() (*DataReceiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
 	}
+	return &DataReceiver{
+		data:     make(chan types.OBUData, 128),
+		producer: p,
+	}, nil
+}
+
+func (dr *DataReceiver) dataProducer(data types.OBUData) error {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	// start a goroutine the check if the data was delivered or not
+	go func() {
+		for e := range dr.producer.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+	topic := KafkaTopic
+	err = dr.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          bytes,
+	}, nil)
+	return err
 }
 
 type DataReceiver struct {
-	data chan types.OBUData
-	conn *websocket.Conn
+	data     chan types.OBUData
+	conn     *websocket.Conn
+	producer *kafka.Producer
 }
 
 func (dr *DataReceiver) wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -51,7 +91,8 @@ func (dr *DataReceiver) wsReceiverLoop() {
 			log.Fatal("something wrong", err)
 			continue
 		}
-		fmt.Printf("received Obu Data: [%d] :: <lat %.2f  long %.2f> \n", obuData.OBUID, obuData.Latitude, obuData.Longitude)
-		dr.data <- obuData
+		//fmt.Printf("received Obu Data: [%d] :: <lat %.2f  long %.2f> \n", obuData.OBUID, obuData.Latitude, obuData.Longitude)
+		//dr.data <- obuData
+		fmt.Println("message reciveded", obuData)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/track-tracking
 
 go 1.20
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.2.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
